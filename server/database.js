f4b1cf8f108c079eb6d7a996d980d38bcc616f05
ql <q728@foxmail.com>
2023-05-09 14:27:01 +0800

    updata .gitignore

--- server/database.js
+++ server/database.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const db_post=mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'...',
+    password:'516..',
     database:'post',
     timezone:"SYSTEM"
 });
@@ -14,7 +14,7 @@ const db_post=mysql.createConnection({
 const db_account=mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'...',
+    password:'516..',
     database:'account',
     timezone:"SYSTEM"
 });
